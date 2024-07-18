f28441d7e695e5565f62be9c469879b666e4d047
STEPHR <rene.r.stephan@mercedes-benz.com>
2024-07-18 14:26:10 +0200

    refactor: remove todo

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    //   TODO Add Scope Enum Here
-    // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
+
     'type-enum': [
       2,
       'always',
