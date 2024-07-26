216e0790bbcc0729b70481990de840ef1dc31d42
STEPHR <rene.r.stephan@mercedes-benz.com>
2024-07-26 07:30:09 +0200

    feat: utility class to generate color css classes by type and saturation

---
+++ SlottyMedia/Components/Partial/Enum/ColorWay.cs
@@ -0,0 +1,19 @@
+namespace SlottyMedia.Components.Partial.Enum;
+
+/// <summary>
+/// Represents color classes defined in our tailwind.config.js
+/// </summary>
+public enum ColorWay
+{
+    Transparent,
+    AthensGray,
+    SavoyBlue,
+    Grape,
+    Fandango,
+    CrayolaOrange,
+    Jasmine,
+    Black,
+    RaisinBlack,
+    CadetGray,
+    White
+}
