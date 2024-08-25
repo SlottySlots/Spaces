3a86dcd872790202af1365b6ec84a397fc6fd044
deadmade <130925709+deadmade@users.noreply.github.com>
2024-08-25 09:56:35 +0200

    fix: fixed friends count

--- SlottyMedia/Backend/Services/UserService.cs
+++ SlottyMedia/Backend/Services/UserService.cs
@@ -307,7 +307,7 @@ public class UserService : IUserService
         try
         {
             Logger.LogInfo($"Fetching friends count for user with ID {userId}");
-            var friends = await _databaseActions.GetCountByField<UserDao>("userID", userId.ToString());
+            var friends = await _databaseActions.GetCountByField<FollowerUserRelationDao>("userIsFollowed", userId.ToString());
             return friends;
         }
         catch (GeneralDatabaseException ex)
