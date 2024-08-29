eca41f7c8dab6bcc3fc6aa5030e5cb7b949359f9
Mario Di Caprio <mariodicaprio10@gmail.com>
2024-08-29 23:31:40 +0200

    feat(posts): added feature to load more posts on demand in home page

--- SlottyMedia/Backend/ViewModel/HomePageVmImpl.cs
+++ SlottyMedia/Backend/ViewModel/HomePageVmImpl.cs
@@ -4,20 +4,51 @@ using SlottyMedia.Backend.ViewModel.Interfaces;
 
 namespace SlottyMedia.Backend.ViewModel;
 
+
+/// <inheritdoc />
 public class HomePageVmImpl : IHomePageVm
 {
     private readonly IPostService _postService;
 
+    private int _currentPostPage;
+
+    /// <summary>Instantiates this class</summary>
     public HomePageVmImpl(IPostService postService)
     {
         _postService = postService;
     }
 
-    public List<PostDto> Posts { get; set; } = new();
+    /// <inheritdoc />
+    public bool IsLoadingPage { get; private set; }
 
-    public async Task FetchPosts()
+    /// <inheritdoc />
+    public bool IsLoadingPosts { get; private set; }
+
+    /// <inheritdoc />
+    public List<PostDto> Posts { get; private set; } = [];
+
+    /// <inheritdoc />
+    public int TotalNumberOfPosts { get; private set; }
+
+    /// <inheritdoc />
+    public async Task Initialize()
     {
-        Posts.Clear();
-        Posts = await _postService.GetAllPosts(1);
+        IsLoadingPage = true;
+        Posts = [];
+        _currentPostPage = 0;
+        TotalNumberOfPosts = await _postService.CountAllPosts();
+        IsLoadingPage = false;
+        await LoadMorePosts();
+    }
+
+    /// <inheritdoc />
+    public async Task LoadMorePosts()
+    {
+        IsLoadingPosts = true;
+        _currentPostPage++;
+        var results = await _postService.GetAllPosts(_currentPostPage);
+        foreach (var post in results)
+            Posts.Add(post);
+        IsLoadingPage = false;
     }
 }
--- SlottyMedia/Backend/ViewModel/Interfaces/IHomePageVm.cs
+++ SlottyMedia/Backend/ViewModel/Interfaces/IHomePageVm.cs
@@ -1,10 +1,34 @@
 using SlottyMedia.Backend.Dtos;
+using SlottyMedia.Components.Pages;
 
 namespace SlottyMedia.Backend.ViewModel.Interfaces;
 
+
+/// <summary>
+///     This ViewModel represents the state of the <see cref="Home"/> page.
+/// </summary>
 public interface IHomePageVm
 {
-    List<PostDto> Posts { get; set; }
+    /// <summary>Indicates whether the page is loading (for the first time)</summary>
+    bool IsLoadingPage { get; }
+    
+    /// <summary>Indicates whether more posts are currently being loaded</summary>
+    bool IsLoadingPosts { get; }
+    
+    /// <summary>The posts that will be showcased</summary>
+    List<PostDto> Posts { get; }
+    
+    /// <summary>The total number of existing posts. Used to enable the user to load more posts on demand.</summary>
+    int TotalNumberOfPosts { get; }
 
-    Task FetchPosts();
+    /// <summary>
+    ///     Initializes this ViewModel, which counts the total number of existing posts and loads the first few
+    ///     posts into the view.
+    /// </summary>
+    Task Initialize();
+
+    /// <summary>
+    ///     Loads more posts to the view. Does nothing if all posts have already been fetched.
+    /// </summary>
+    Task LoadMorePosts();
 }
